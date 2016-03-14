32d32291329dd4bc581d5bbd40d5f6ae3976b65a
FrostOne <tansuongkevin@gmail.com>
2016-03-14 21:29:55 +0100

    Adding a little woah effect in selection choices
    Making the circle tingling according to the user choice.

--- src/js/choice.js
+++ src/js/choice.js
@@ -6,28 +6,29 @@ var choiceA = document.querySelector('.choiceA');
 var choiceB = document.querySelector('.choiceB');
 var choiceC = document.querySelector('.choiceC');
 var choiceD = document.querySelector('.choiceD');
-
-
-
+var circle_pulsar = document.querySelector('.circle_pulsar');
 
 choiceA.addEventListener('click', function(){
 		arrow_up.classList.toggle('onpress');
 		choiceA.classList.toggle('selected');
+		circle_pulsar.classList.toggle('animation_top');
+
 });
 choiceB.addEventListener('click', function(){
 		arrow_right.classList.toggle('onpress');
 		choiceB.classList.toggle('selected');
+		circle_pulsar.classList.toggle('animation_right');
 });
 choiceC.addEventListener('click', function(){
 		arrow_bottom.classList.toggle('onpress');
 		choiceC.classList.toggle('selected');
+		circle_pulsar.classList.toggle('animation_bottom');
 });
 choiceD.addEventListener('click', function(){
 		arrow_left.classList.toggle('onpress');
 		choiceD.classList.toggle('selected');
+		circle_pulsar.classList.toggle('animation_left');
 });
-
-
 
 window.addEventListener('keydown', function(e){
 	console.log(e.keyCode);
@@ -36,21 +37,25 @@ window.addEventListener('keydown', function(e){
 		console.log('UP');
 		arrow_up.classList.toggle('onpress');
 		choiceA.classList.toggle('selected');
+		circle_pulsar.classList.toggle('animation_top');
 		break;
 		case 39:
 		console.log('RIGHT');
 		arrow_right.classList.toggle('onpress');
 		choiceB.classList.toggle('selected');
+		circle_pulsar.classList.toggle('animation_right');
 		break;
 		case 37:
 		console.log('LEFT');
 		arrow_left.classList.toggle('onpress');
 		choiceD.classList.toggle('selected');
+		circle_pulsar.classList.toggle('animation_left');
 		break;
 		case 40:
 		console.log('DOWN');
 		arrow_bottom.classList.toggle('onpress');
 		choiceC.classList.toggle('selected');
+		circle_pulsar.classList.toggle('animation_bottom');
 		break;
 		default:
 		break;
