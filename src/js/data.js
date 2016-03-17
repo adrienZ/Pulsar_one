2a8f1f09db28a954ebeb89029ba728547062e95e
Jennyfer-M <jennymillet@gmail.com>
2016-03-17 08:30:06 +0100

    Data modif

--- src/js/data.js
+++ src/js/data.js
@@ -151,12 +151,9 @@ var data = {
         },
 
         'a1_4': {
-<<<<<<< HEAD
             "question": "JEU CHOIX DU CHIFFRE POUR AVOIR LE BUS",
-=======
             "question": "JEU CHOIX DU CHIFFRE",
             mini_game: 'personality_test',
->>>>>>> origin/master
             "choix": [
                 {
                     "text": "",
